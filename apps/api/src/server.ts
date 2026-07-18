df70ab0bab8425b54f3f1e62b322a99a1afb5d48
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-07-18 12:06:54 +0100

    refactor(api): rewrite the api initialisation with logs and environment variables

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,13 +1,35 @@
 import { buildApp } from "./app";
 
-const app = buildApp();
+const HOST = process.env.HOST || "0.0.0.0";
+const PORT = Number(process.env.PORT) || 3000;
 
-try {
-  await app.listen({
-    port: 3000,
-    host: "0.0.0.0",
-  });
-} catch (error) {
-  app.log.error(error);
-  process.exit(1);
+async function start() {
+  const app = await buildApp();
+
+  try {
+    await app.listen({ port: PORT, host: HOST });
+
+    const signals = ["SIGINT", "SIGTERM"];
+
+    for (const signal of signals) {
+      process.on(signal, async () => {
+        app.log.info(`Received ${signal}. Shutting down the application safely...`);
+
+        try {
+          await app.close();
+          app.log.info("Application shut down successfully.");
+          process.exit(0);
+        } catch (err: unknown) {
+          app.log.error(`Error closing the application: ${err}`);
+          process.exit(1);
+        }
+      });
+    }
+
+  } catch (error) {
+    console.error("An error occurred whilst launching the application: ", error);
+    process.exit(1);
+  }
 }
+
+start();
