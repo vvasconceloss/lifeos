4ed1e7bb332366c26021f52131b32b751c67d5e9
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-07-28 20:56:38 +0100

    feat(api): register cookie, JWT and CSRF plugins

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,4 +1,8 @@
+import 'dotenv/config';
+import { jwtPlugin } from './plugins/jwt';
 import { corsPlugin } from './plugins/cors';
+import { csrfPlugin } from './plugins/csrf';
+import { cookiesPlugin } from './plugins/cookie';
 import { systemRoutes } from './routes/system.routes';
 import Fastify, { type FastifyInstance } from 'fastify';
 
@@ -7,7 +11,11 @@ export async function buildApp(): Promise<FastifyInstance> {
     logger: true
   });
 
+  await fastify.register(jwtPlugin);
   await fastify.register(corsPlugin);
+  await fastify.register(csrfPlugin);
+  await fastify.register(cookiesPlugin);
+
   await fastify.register(systemRoutes, { prefix: '/v1' });
 
   return fastify;
