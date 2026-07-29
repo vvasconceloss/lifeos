91f5d8bdcc7c486fe68f462ccff77a269f88f2bc
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-07-29 12:07:45 +0100

    chore(web): add AuthProvider + AppRouter

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,8 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
+import { StrictMode } from 'react'
+import { AppRouter } from './router'
+import { createRoot } from 'react-dom/client'
+import { AuthProvider } from './contexts/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
   </StrictMode>,
 )
